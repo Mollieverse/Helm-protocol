f686977141dd0f2f557ab52cdba16b23194ab4da
Mollie <imaobongdominic16@gmail.com>
2026-04-23 07:33:39 +0100

    Add files via upload

---
+++ providers.tsx
@@ -0,0 +1,26 @@
+'use client';
+import React, { useMemo } from 'react';
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
+import { SOLANA_RPC_ENDPOINT } from '@/lib/constants';
+
+// Import wallet adapter CSS
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+export function SolanaProviders({ children }: { children: React.ReactNode }) {
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint={SOLANA_RPC_ENDPOINT}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          {children}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
